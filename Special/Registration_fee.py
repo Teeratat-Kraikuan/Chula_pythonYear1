4b131e08794add3296a9fe1c6ec2aa52989b5d05
Teeratat Kraikuan <teeratach043@gmail.com>
2022-02-10 12:34:09 +0700

    Special 'Registration free'

---
+++ Special/Registration_fee.py
@@ -0,0 +1,44 @@
+def invalid_id(id):
+    return (len(id) != 10 or int(id[:2]) < 50 or id[2] not in ['3','4','7'] or int(id[8:]) < 21 or int(id[8:]) > 27)
+
+def calculate_price(id, sem):
+    prices = [[[[['18,000'], ['21,000']], [['4,500'], ['5,250']]], [[['26,000'], ['31,000']], [['7,000'], ['7,750']]]], \
+              [[[['14,500'], ['17,000']], [['4,500'], ['5,250']]], [[['19,000'], ['23,000']], [['7,000'], ['7,750']]]]]
+    
+    # a is Group
+    # b is Bachelor or Graduated
+    # c is sem
+    # d is the first 2 char in id
+    if id[8:] in ['21', '23', '25', '28']:
+        a = 0
+    else:
+        a = 1
+    if id[2] in ['3', '4']:
+        b = 0
+    else:
+        b = 1
+    if sem == 1 or sem == 2:
+        c = 0
+    else:
+        c = 1
+    if int(id[:2]) < 56:
+        d = 0
+    else:
+        d = 1
+    return prices[a][b][c][d][0]
+
+def main():
+    id = input("Enter student ID : ")
+    if invalid_id(id):
+        print("Invalid ID")
+        return
+    
+    sem = int(input("Enter semester : "))
+    if sem < 1 or sem > 3:
+        print("Invalid semester")
+        return    
+    
+    price = calculate_price(id, sem)
+    print("Registration fee : " + price)
+    
+main()
